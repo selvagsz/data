e9b536eb026da91061479cc3f951d385edafe8f1
Chris Thoburn <cthoburn@linkedin.com>
2019-11-26 02:58:00 -0800

    all the types and most the docs

--- packages/store/addon/-private/ts-interfaces/minimum-adapter-interface.ts
+++ packages/store/addon/-private/ts-interfaces/minimum-adapter-interface.ts
@@ -2,10 +2,12 @@ type Snapshot = import('../system/snapshot').default;
 type SnapshotRecordArray = import('../system/snapshot-record-array').default;
 type Store = import('../system/core-store').default;
 type ModelSchema = import('../ts-interfaces/ds-model').ModelSchema;
-
+type AdapterPopulatedRecordArray = import('../system/record-arrays/adapter-populated-record-array').default;
+type RelationshipSchema = import('./record-data-schemas').RelationshipSchema;
 import { Dict } from './utils';
 
-type OptionsHash = Dict<any>;
+type Group = Snapshot[];
+
 /**
   ## Overview
 
@@ -98,11 +100,11 @@ type OptionsHash = Dict<any>;
   @class MinimumAdapterInterface
   @public
 */
-type Group = Snapshot[];
 interface Adapter {
   /**
    * `adapter.findRecord` takes a request for a resource of a given `type` and `id` combination
-   * and should return a promise which fulfills with resource data matching that `type` and `id`.
+   * and should return a `Promise` which fulfills with data for a single resource matching that
+   * `type` and `id`.
    *
    * The response will be fed to the associated serializer's `normalizeResponse` method with the
    * `requestType` set to `findRecord`.
@@ -110,38 +112,236 @@ interface Adapter {
    * `adapter.findRecord` is called whenever the `store` needs to load, reload, or backgroundReload
    * the resource data for a given `type` and `id`.
    *
+   * ⚠️ If the adapter's response resolves to a false-y value, the associated `serializer.normalizeResponse`
+   * call will NOT be made. In this scenario you may need to do at least a minimum amount of response
+   * processing within the adapter.
+   *
    * @method findRecord
+   * @public
    * @param {Store} store
    * @param {ModelSchema} schema
    * @param {String} id
    * @param {Snapshot} snapshot
+   * @return {Promise} a promise resolving with resource data to feed to the associated serializer
    */
   findRecord(store: Store, schema: ModelSchema, id: string, snapshot: Snapshot): Promise<unknown>;
 
   /**
+   * `adapter.findAll` takes a request for resources of a given `type` and should return
+   *  a `Promise` which fulfills with a collection of resource data matching that `type`.
    *
+   * The response will be fed to the associated serializer's `normalizeResponse` method
+   *  with the `requestType` set to `findAll`.
+   *
+   * `adapter.findAll` is called whenever `store.findAll` is asked to reload or backgroundReload.
+   * The records in the response are merged with the contents of the store. Existing records for
+   * the `type` will not be removed.
+   *
+   * ⚠️ If the adapter's response resolves to a false-y value, the associated `serializer.normalizeResponse`
+   * call will NOT be made. In this scenario you may need to do at least a minimum amount of response
+   * processing within the adapter.
+   *
+   * @method findAll
+   * @public
    * @param {Store} store
    * @param {ModelSchema} schema
    * @param {null} sinceToken This parameter is no longer used and will always be null.
-   * @param snapshotRecordArray
+   * @param {SnapshotRecordArray} snapshotRecordArray an object containing any passed in options,
+   *  adapterOptions, and the ability to access a snapshot for each existing record of the type.
+   * @return {Promise} a promise resolving with resource data to feed to the associated serializer
    */
-  findAll(store: Store, schema: ModelSchema, sinceToken: null, snapshotRecordArray): Promise<unknown>;
-  query(Store: Store, schema: ModelSchema, query, recordArray, options): Promise<unknown>;
-  queryRecord(store: Store, schema: ModelSchema, query, options): Promise<unknown>;
+  findAll(
+    store: Store,
+    schema: ModelSchema,
+    sinceToken: null,
+    snapshotRecordArray: SnapshotRecordArray
+  ): Promise<unknown>;
 
+  /**
+   * `adapter.query` takes a request for resources of a given `type` and should return
+   *  a `Promise` which fulfills with a collection of resource data matching that `type`.
+   *
+   * The response will be fed to the associated serializer's `normalizeResponse` method
+   *  with the `requestType` set to `query`.
+   *
+   * `adapter.query` is called whenever `store.query` is called or a previous query result is
+   * asked to reload.
+   *
+   * As with `findAll`, the records in the response are merged with the contents of the store.
+   * Existing records for the `type` will not be removed. The key difference is in the result
+   * returned by the `store`. For `findAll` the result is all known records of the `type`,
+   * while for `query` it will only be the records returned from `adapter.query`.
+   *
+   * ⚠️ If the adapter's response resolves to a false-y value, the associated `serializer.normalizeResponse`
+   * call will NOT be made. In this scenario you may need to do at least a minimum amount of response
+   * processing within the adapter.
+   *
+   * @method query
+   * @public
+   * @param {Store} store
+   * @param {ModelSchema} schema
+   * @param {object} query
+   * @param {AdapterPopulatedRecordArray} recordArray
+   * @param {object} options
+   * @return {Promise} a promise resolving with resource data to feed to the associated serializer
+   */
+  query(
+    store: Store,
+    schema: ModelSchema,
+    query: Dict<any>,
+    recordArray: AdapterPopulatedRecordArray,
+    options: { adapterOptions?: any }
+  ): Promise<unknown>;
+
+  /**
+   * `adapter.queryRecord` takes a request for resource of a given `type` and should return
+   *  a `Promise` which fulfills with data for a single resource matching that `type`.
+   *
+   * The response will be fed to the associated serializer's `normalizeResponse` method
+   *  with the `requestType` set to `queryRecord`.
+   *
+   * ⚠️ If the adapter's response resolves to a false-y value, the associated `serializer.normalizeResponse`
+   * call will NOT be made. In this scenario you may need to do at least a minimum amount of response
+   * processing within the adapter.
+   *
+   * @method queryRecord
+   * @public
+   * @param {Store} store
+   * @param {ModelSchema} schema
+   * @param query
+   * @param options
+   * @return {Promise} a promise resolving with resource data to feed to the associated serializer
+   */
+  queryRecord(store: Store, schema: ModelSchema, query: Dict<any>, options: { adapterOptions?: any }): Promise<unknown>;
+
+  /**
+   * `adapter.createRecord` takes a request to create a resource of a given `type` and should
+   * return a `Promise` which fulfills with data for the newly created resource.
+   *
+   * The response will be fed to the associated serializer's `normalizeResponse` method
+   *  with the `requestType` set to `createRecord`.
+   *
+   * ⚠️ If the adapter's response resolves to a false-y value, the associated `serializer.normalizeResponse`
+   * call will NOT be made. In this scenario you may need to do at least a minimum amount of response
+   * processing within the adapter.
+   *
+   * @param store
+   * @param schema
+   * @param snapshot
+   * @return {Promise} a promise resolving with resource data to feed to the associated serializer
+   */
   createRecord(store: Store, schema: ModelSchema, snapshot: Snapshot): Promise<unknown>;
+
+  /**
+   * `adapter.updateRecord` takes a request to update a resource of a given `type` and should
+   * return a `Promise` which fulfills with the updated data for the resource.
+   *
+   * The response will be fed to the associated serializer's `normalizeResponse` method
+   *  with the `requestType` set to `updateRecord`.
+   *
+   * ⚠️ If the adapter's response resolves to a false-y value, the associated `serializer.normalizeResponse`
+   * call will NOT be made. In this scenario you may need to do at least a minimum amount of response
+   * processing within the adapter.
+   *
+   * @param store
+   * @param schema
+   * @param snapshot
+   */
   updateRecord(store: Store, schema: ModelSchema, snapshot: Snapshot): Promise<unknown>;
+
+  /**
+   * `adapter.deleteRecord` takes a request to delete a resource of a given `type` and
+   * should return a `Promise` which resolves when that deletion is complete.
+   *
+   * Usually the response will be empty, but you may include additional updates in the
+   * response.
+   *
+   * The response will be fed to the associated serializer's `normalizeResponse` method
+   *  with the `requestType` set to `deleteRecord`.
+   *
+   * ⚠️ If the adapter's response resolves to a false-y value, the associated `serializer.normalizeResponse`
+   * call will NOT be made. In this scenario you may need to do at least a minimum amount of response
+   * processing within the adapter.
+   *
+   * @param store
+   * @param schema
+   * @param snapshot
+   */
   deleteRecord(store: Store, schema: ModelSchema, snapshot: Snapshot): Promise<unknown>;
 
-  findBelongsTo?(store: Store, snapshot: Snapshot, relatedLink: string, relationship): Promise<unknown>;
-  findHasMany?(store: Store, snapshot: Snapshot, relatedLink: string, relationship): Promise<unknown>;
+  /**
+   *
+   * The response will be fed to the associated serializer's `normalizeResponse` method
+   *  with the `requestType` set to `findBelongsTo`.
+   *
+   * ⚠️ If the adapter's response resolves to a false-y value, the associated `serializer.normalizeResponse`
+   * call will NOT be made. In this scenario you may need to do at least a minimum amount of response
+   * processing within the adapter.
+   *
+   * @method findBelongsTo [OPTIONAL]
+   * @public
+   * @optional
+   * @param {Store} store
+   * @param {Snapshot} snapshot
+   * @param {string} relatedLink
+   * @param {object} relationship
+   * @return {Promise} a promise resolving with resource data to feed to the associated serializer
+   */
+  findBelongsTo?(
+    store: Store,
+    snapshot: Snapshot,
+    relatedLink: string,
+    relationship: RelationshipSchema
+  ): Promise<unknown>;
+
+  /**
+   *
+   * The response will be fed to the associated serializer's `normalizeResponse` method
+   *  with the `requestType` set to `findHasMany`.
+   *
+   * ⚠️ If the adapter's response resolves to a false-y value, the associated `serializer.normalizeResponse`
+   * call will NOT be made. In this scenario you may need to do at least a minimum amount of response
+   * processing within the adapter.
+   *
+   * @method findhasMany [OPTIONAL]
+   * @public
+   * @optional
+   * @param {Store} store
+   * @param {Snapshot} snapshot
+   * @param {string} relatedLink
+   * @param {object} relationship
+   * @return {Promise} a promise resolving with resource data to feed to the associated serializer
+   */
+  findHasMany?(
+    store: Store,
+    snapshot: Snapshot,
+    relatedLink: string,
+    relationship: RelationshipSchema
+  ): Promise<unknown>;
+
+  /**
+   *
+   * The response will be fed to the associated serializer's `normalizeResponse` method
+   *  with the `requestType` set to `findMany`.
+   *
+   * ⚠️ If the adapter's response resolves to a false-y value, the associated `serializer.normalizeResponse`
+   * call will NOT be made. In this scenario you may need to do at least a minimum amount of response
+   * processing within the adapter.
+   *
+   * @param store
+   * @param schema
+   * @param ids
+   * @param snapshots
+   */
+  findMany?(store: Store, schema: ModelSchema, ids: string[], snapshots: Snapshot[]): Promise<unknown>;
+
   generateIdForRecord?(store: Store, modelName: string, properties: unknown): string;
-  findMany?(store: Store, schema: ModelSchema, ids: string[], snapshots): Promise<unknown>;
-  groupRecordsForFindMany?(store: Store, snapshots): Group[];
+  groupRecordsForFindMany?(store: Store, snapshots: Snapshot[]): Group[];
+
   shouldReloadRecord?(store: Store, snapshot: Snapshot): boolean;
-  shouldReloadAll?(store: Store, snapshotArray): boolean;
+  shouldReloadAll?(store: Store, snapshotArray: SnapshotRecordArray): boolean;
   shouldBackgroundReloadRecord?(store: Store, snapshot: Snapshot): boolean;
-  shouldBackgroundReloadAll?(store: Store, snapshotArray): boolean;
+  shouldBackgroundReloadAll?(store: Store, snapshotArray: SnapshotRecordArray): boolean;
 
   coalesceFindRequests?: boolean;
 }
